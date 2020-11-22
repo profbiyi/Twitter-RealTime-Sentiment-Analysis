0298aea204b4d1a8e31ca0fb5818ad26dd82036a
profbiyi <ahmed.oladapo@gmail.com>
2020-11-22 09:15:04 +0100

    Update apiConfig.py
    remove keys from config

--- src/apiConfig.py
+++ src/apiConfig.py
@@ -1,5 +1,5 @@
 # put your keys and secret here
-access_token = "175896445-D114K70gBlHLGGSP7hFe3yRiekmzfBWetNO8xzEP"
-access_token_secret = "QePNp06ULlZpzNQTj1RoH6Of6F7WuOetGjx1sauU8kJQz"
-consumer_key = "1OhHPGgxBPP0rA5u772Sbn8Wh"
-consumer_secret = "N2W4q67hXpL831HqHz3uDmaIxCAPfDRtS7FZW9i0TXKlbIbwx7"
+access_token = ""
+access_token_secret = ""
+consumer_key = ""
+consumer_secret = ""
