b842023c983ac417270b47de19ae23c12c26fbff
profbiyi <ahmed.oladapo@gmail.com>
2020-11-28 00:44:22 +0100

    removed credentials

--- src/apiConfig.py
+++ src/apiConfig.py
@@ -1,13 +1,13 @@
 # put your keys and secret here
-access_token = "175896445-D114K70gBlHLGGSP7hFe3yRiekmzfBWetNO8xzEP"
-access_token_secret = "QePNp06ULlZpzNQTj1RoH6Of6F7WuOetGjx1sauU8kJQz"
-consumer_key = "1OhHPGgxBPP0rA5u772Sbn8Wh"
-consumer_secret = "N2W4q67hXpL831HqHz3uDmaIxCAPfDRtS7FZW9i0TXKlbIbwx7"
+access_token = ""
+access_token_secret = ""
+consumer_key = ""
+consumer_secret = ""
 
 
 
-user = "postgres"
-password = "harbey2591"
-host = "pgdatabase.cyyfwzjjp4bm.us-east-1.rds.amazonaws.com"
+user = ""
+password = ""
+host = ""
 port = "5432"
-database =  "postgres"
+database =  ""
